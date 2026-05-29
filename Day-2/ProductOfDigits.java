76c309ad05cd1acdc36af7eab595802d462c07aa
J Himanshu Achary <111566076+Himanshuff@users.noreply.github.com>
2026-05-29 23:24:17 +0530

    Update ProductOfDigits.java

--- Day-2/ProductOfDigits.java
+++ Day-2/ProductOfDigits.java
@@ -5,20 +5,18 @@ public class ProductOfDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input number
+        
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
         int product = 1;
 
-        // Find product of digits
         while (num != 0) {
             int digit = num % 10;
             product = product * digit;
             num = num / 10;
         }
 
-        // Display result
         System.out.println("Product of digits = " + product);
 
         sc.close();
