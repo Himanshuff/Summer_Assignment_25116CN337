c981ab834fae8ebfeca6c245faacd80849b3de29
J Himanshu Achary <111566076+Himanshuff@users.noreply.github.com>
2026-05-29 23:23:34 +0530

    Update SumOfDigits.java

--- Day-2/SumOfDigits.java
+++ Day-2/SumOfDigits.java
@@ -5,20 +5,20 @@ public class SumOfDigits {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input number
+        
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
         int sum = 0;
 
-        // Find sum of digits
+        
         while (num != 0) {
             int digit = num % 10;
             sum = sum + digit;
             num = num / 10;
         }
 
-        // Display result
+        
         System.out.println("Sum of digits = " + sum);
 
         sc.close();
