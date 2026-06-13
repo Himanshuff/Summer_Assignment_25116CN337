044e8db52cc219d3b84da8b5cc4ebfc6cae17ead
Himanshu <himanshuachary2007@gmail.com>
2026-06-13 22:02:17 +0530

    Day-6 uploaded

---
+++ Day-6/CountSetBits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CountSetBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        while (num > 0) {
+            count += num & 1; // Check last bit
+            num >>= 1;        // Right shift by 1
+        }
+
+        System.out.println("Number of set bits: " + count);
+
+        sc.close();
+    }
+}
