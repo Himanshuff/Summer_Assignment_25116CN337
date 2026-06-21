d6ba0343c76ad84a3b9f6bc7d7a437e0eefdb07a
Himanshu <himanshuachary2007@gmail.com>
2026-06-21 21:14:34 +0530

    Day-11 uploaded

---
+++ Day-11/MaximumNumber.java
@@ -0,0 +1,19 @@
+public class MaximumNumber {
+
+    
+    static int findMax(int a, int b) {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 40;
+
+        int max = findMax(num1, num2);
+
+        System.out.println("Maximum number = " + max);
+    }
+}
---
+++ Day-11/SumOfTwoNumbers.java
@@ -0,0 +1,16 @@
+public class SumOfTwoNumbers {
+
+    
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int result = sum(num1, num2);
+
+        System.out.println("Sum = " + result);
+    }
+}
